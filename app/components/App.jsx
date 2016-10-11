e22967dad5b44ee7b8b666c3468b08547546b230
helvetic <my@maggit.ru>
2016-10-11 17:33:49 +0300

    init

---
+++ app/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// import some from './some.js';
+import style from './some.scss';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div id="content">
+        <h1>&nbsp;</h1>
+        <h2>Welcome!</h2>
+        <ul className={style.title}>
+          <li><a href="http://brunch.io">Brunch homepage</a></li>
+          <li><a href="https://facebook.github.io/react/">React.js homepage</a></li>
+        </ul>
+      </div>
+    );
+  }
+}
---
+++ brunch-config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: {
+        'vendor.js': /^(?!app)/,
+        'app.js': /^app/
+      }
+    },
+    stylesheets: {
+      joinTo: 'css/app.css',
+      order: {
+        before: ['vendor/styles/normalize.css']
+      }
+    }
+  },
+
+  plugins: {
+    babel: {presets: ['es2015', 'react']},
+    sass: {
+      mode: 'native',
+      allowCache: true,
+      modules: true
+    }
+  }
+};
